626a57732f5f6ac028066cbfe0c2fc2058a4c082
Railson25 <railsonsantiago123@gmail.com>
2020-02-14 13:28:18 -0300

    Relação entre Componentes

---
+++ src/componentes/FamiliaSilva.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Membro from './Membro'
+
+export default props => 
+    <div>
+        <Membro nome='Rafael' sobrenome='Silva' />
+        <Membro nome='Daniela' sobrenome='Silva' />
+        <Membro nome='Pedro' sobrenome='Silva' />
+        <Membro nome='Ana' sobrenome='Silva' />
+    </div>
+
+//Acessando um componente através de outro Componente
--- src/index.js
+++ src/index.js
@@ -3,14 +3,29 @@ import React from 'react'
 
 //import PrimeiroComponente from './componentes/PrimeiroComponente'
 //import { CompA, CompB } from './componentes/DoisComponentes'
-import MultiElementos from './componentes/MultiElementos'
+//import MultiElementos from './componentes/MultiElementos'
+//import FamiliaSilva from './componentes/FamiliaSilva'
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
 <div>
-    <MultiElementos  />
-    {/* <CompA valor='Olá eu sou A' />
+    <Familia>
+        <Membro nome='Andre' sobrenome='Pereira' />
+        <Membro nome='Mariana' sobrenome='Pereira' />
+    </Familia>
+    <Familia>
+        <Membro nome='Bia' sobrenome='Martins' />
+        <Membro nome='Gustavo' sobrenome='Martins' />
+    </Familia>
+    {/*<FamiliaSilva />
+     <MultiElementos  />
+     <CompA valor='Olá eu sou A' />
     <CompB valor='Olá eu sou B' />
     <PrimeiroComponente valor='Bom dia !'/> */}
 </div>
 , elemento)
+
+//Para instancias diretamente no componente 
+//Precisa importa o Componente 
