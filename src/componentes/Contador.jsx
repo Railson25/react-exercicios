9652330509cba2409491259d0a2a5cd733c04bb7
Railson25 <railsonsantiago123@gmail.com>
2020-02-29 10:40:10 -0300

    Resolvendo Problema do This

--- src/componentes/Contador.jsx
+++ src/componentes/Contador.jsx
@@ -1,20 +1,37 @@
 import  React, { Component } from 'react' 
 
-export default class Contador extends Component {
-    maisUm() {
+export default class Contador extends Component {    
+    
+    maisUm = () => {
         this.props.numero++
-         // acrescentar uma inidade a props numero 
-         //this não está apontando para o Contador 
-    }    
-
+        
+    
     render() {
         return (
             <div>
                 <div>Número: {this.props.numero}</div>
                 <button onClick={this.maisUm}>Inc</button>
-                {/* Dispara a função ao clicar */}
                 <button>Dec</button>
             </div>
         )
     }
-} 
+}
+
+/*Solução  01 - Bind
+constructor(props){
+    super(props)
+    this.maisUm = this.maisUm.bind(this)
+    no contexto do construtor sendo chamado sempre o this
+    vai apontar para uma instancia de classe
+*/
+
+//Solução 02 - onClick
+//<button onClick={() => this.maisUm()}>Inc</button>
+//utillizar arrow functions e chamar a função
+// o this apontará para a classe
+
+//Solução 03 
+//maisUm = () => {
+//    this.props.numero++
+//} 
+// tranformar a função maisUm em arrow   
